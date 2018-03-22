c23a8a6b3ea330702fdff0e7632cc95a919acd02
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-03-22 08:59:33 +0100

    Add files via upload

---
+++ CarteChance.java
@@ -0,0 +1,118 @@
+import java.awt.* ;
+import javax.swing.*;
+import java.util.ArrayList;
+//il faudrait créer une classe CarteChance par type de carte chance 
+//méthodes avec toutes les actions et on peut faire un choix aléatoire ensuite de la méthode 
+//on peut mettre plus de poids pour certaines actions 
+//case possède un getJoueur qui donne le joueur qui se reouvent sur la case 
+//mettre méthode JPanel dans chaque actions différentes pour le panel de droite 
+public class CarteChance {
+	
+	public String texteAffiche = " CARTE CHANCE " ;
+	public String description ; 
+	public JPanel panelCarteChance = new JPanel(new BorderLayout());
+	
+	public Joueur joueur ;
+	//liste d'entiers qui permet de faire le tirage au sort de la méthode qui va s'appliquer au tirage 
+	ArrayList<Integer> listCart = new ArrayList<Integer>() ; 
+	int i = 0 ; //va donner la valeur de la méthode utilisée 
+	
+	//quand un joueur arrive sur une case il va créer une carte chance avec en attribut se joueur pour que les méthodes puisse s'executer 
+	public CarteChance(Joueur j){
+		description = "" ; 
+		joueur = j ;
+ 
+		/* 1: sortir de prison (20%)
+		 * 2: aller en prison (25%)
+		 * 3: facture (25%)
+		 * 4: relancer dés (25%)
+		 * 5: mourir(5%) 
+		 */
+		 for(int i =0; i <20 ; i ++){
+			 listCart.add(1);
+		 }
+		 for(int i =20; i <45 ; i ++){
+			 listCart.add(2);
+		 }
+		 for(int i =45; i <70 ; i ++){
+			 listCart.add(3);
+		 }
+		 for(int i =70; i <95; i ++){
+			 listCart.add(4);
+		 }
+		 for(int i =95; i <5 ; i ++){
+			 listCart.add(5);
+		 }
+	}
+	
+	public void setJoueur(Joueur j){
+		joueur = j ;
+	}
+	
+	public Joueur getJoueur(){
+		return joueur ;
+	}
+	
+	public String getDescription(){
+		return description ;
+	}
+	
+	public JPanel getPanel(){
+		return panelCarteChance ;
+	}
+	
+	public void tirerCarte(){
+		int random = (int)(Math.random()*listCart.size()) ;
+		i = listCart.get(random); 
+		if(i ==1){
+			this.carteAllerEnPrison();
+		} else if(i == 2){
+			this.carteRelancerDes();
+		} else if (i == 3){
+			this.cartePayerFacture();
+		} else if ( i == 4){
+			this.carteSortirPrison() ;
+		} else {
+			this.carteMourir();
+		}
+		//ajout de la description associée 
+		JLabel d2 = new JLabel(description);
+		JLabel d1 = new JLabel(texteAffiche) ;
+		panelCarteChance.add(d1, BorderLayout.NORTH);
+		panelCarteChance.add(d2, BorderLayout.CENTER);
+	}
+	
+	//une méthode par type de carte chance 
+	//pour l'instant les méthode s'executent toutes seuls mais on pourra peut être faire 
+	public void carteAllerEnPrison(){
+		description = "La malchance fait partie de la vie ... et justement pas de chance, vous allez très certainement passer quelque temps derrière les barreaux prochainement.";
+		joueur.allerEnPrison() ;
+	}
+	
+	public void carteRelancerDes(){
+		description = "Vous pouvez rejouer !";
+		JButton lancerDes = new JButton("Lancer les des");
+		panelCarteChance.add(lancerDes, BorderLayout.SOUTH);
+		lancerDes.addActionListener(new EcouteurLancerLesDes(joueur)) ;
+	}
+	
+	public void cartePayerFacture(){
+		description = "Il faut payer dans la vie, les factures notamment. Merci de verser 1000 monopolies au système de collecte des factures.";
+		JButton payer = new JButton("Payer");
+		payer.addActionListener(new EcouteurPayer(joueur, -1000));
+		panelCarteChance.add(payer, BorderLayout.SOUTH); 
+	}
+	public void carteSortirPrison(){
+		description = "Vous possédez à présent une carte qui vous permet de sortir de prison, vou spourrez l'utiliser quand vous le souhaitez la prochaine fois que vos mauvaises actions vous mèneront derrière les barreaux.";
+		
+		joueur.setCarteSortirPrison(true) ;
+	}
+	
+	public void carteMourir(){
+		description = "Fin de la partie pour vous !";
+		joueur.tuer() ; 
+	}
+	
+	//une méthode tirerCarte qui va sélectionner une méthode parmi les autres ; il faut que les méthodes aient des possibilités différentes de sortir, on fait une liste d'entiers avec 5 valeurs différentes, chacune de ces 5 valeurs correspond à une méthode 
+
+}
---
+++ FenetrePlateau.java
@@ -0,0 +1,176 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+//utiliser grid layout et changer les tailles, commencer par faire des JPanel
+//dans les JPanel il faudra faire dessiner Joueur et dessiner propriétaire
+//pour l'instant dans cette fenêtre il n'y a que la plateau, on peut la transormer en panel plateau qu'on placerait dans la fenêtre de jeu
+public class FenetrePlateau extends JFrame {
+
+   JLabel jlab ;
+   JTextArea jtext ;
+   PanelCase panecase ;
+   JPanel pane ;
+   LinkedList<PanelCase> listepanelcases = new LinkedList<PanelCase>() ;
+
+    public FenetrePlateau() {
+        super("MONOPOLY");
+        
+        //on devrait avoir un liste des panel case, cela pourrait être util
+        
+       
+        pane = new JPanel(new GridLayout(0,8));
+        JPanel pgris = new JPanel();
+        
+        panecase = new PanelCase(Color.GRAY, new JLabel(" CAGNOTTE"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.RED, new JLabel(" Matignon"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.RED, new JLabel(" Malesherbes"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.RED, new JLabel(" Henri-Martin"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.GRAY, new JLabel(" CHANCE"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.YELLOW, new JLabel(" Saint-Honore"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+         panecase = new PanelCase(Color.YELLOW, new JLabel(" La bourse"), this); 
+         listepanelcases.add(panecase);
+        pane.add(panecase); 
+         panecase = new PanelCase(Color.GRAY, new JLabel(" Allez en prison"), this); 
+         listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.ORANGE, new JLabel(" Pigalle"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase);
+        
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		
+		panecase = new PanelCase(Color.GREEN, new JLabel(" Breteuil"), this); 
+		listepanelcases.add(panecase);
+        pane.add(panecase);
+        panecase = new PanelCase(Color.ORANGE, new JLabel(" Saint-Michel"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase);
+        
+        pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		
+		panecase = new PanelCase(Color.GREEN, new JLabel(" Foch"), this); 
+		listepanelcases.add(panecase);
+        pane.add(panecase);
+        panecase = new PanelCase(Color.ORANGE, new JLabel(" Mozart"), this);
+        listepanelcases.add(panecase); 
+        pane.add(panecase);
+        
+        pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		
+		panecase = new PanelCase(Color.GREEN, new JLabel(" Capucines"), this); 
+		listepanelcases.add(panecase);
+        pane.add(panecase);
+        panecase = new PanelCase(Color.GRAY, new JLabel(" TAXE"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase);
+        
+        pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		
+		panecase = new PanelCase(Color.GRAY, new JLabel(" Chance"), this); 
+		listepanelcases.add(panecase);
+        pane.add(panecase);
+        panecase = new PanelCase(Color.PINK, new JLabel(" Paradis"), this);
+        listepanelcases.add(panecase); 
+        pane.add(panecase);
+        
+        pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		
+		panecase = new PanelCase(Color.BLUE, new JLabel(" Champs-Elysees"), this); 
+		listepanelcases.add(panecase);
+        pane.add(panecase);
+        panecase = new PanelCase(Color.PINK, new JLabel(" Neuilly"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase);
+        
+        pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		pane.add(new JPanel());
+		
+		panecase = new PanelCase(Color.BLUE, new JLabel(" Paix"), this); 
+		listepanelcases.add(panecase);
+        pane.add(panecase);
+        
+        panecase = new PanelCase(Color.GRAY, new JLabel(" PRISON"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.CYAN, new JLabel(" La Republique"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.CYAN, new JLabel(" Courcelles"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.CYAN, new JLabel(" Vaugirard"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.GRAY, new JLabel(" CHANCE"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.BLACK, new JLabel(" Lecourbe"), this);
+        listepanelcases.add(panecase); 
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.BLACK, new JLabel(" Belleville"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        panecase = new PanelCase(Color.GRAY, new JLabel(" DEPART"), this); 
+        listepanelcases.add(panecase);
+        pane.add(panecase); 
+        
+        //test affichage joueur
+        listepanelcases.get(10).dessinerJoueur() ; 
+        listepanelcases.get(10).dessinerJoueur2() ; 
+        
+
+        
+       this.setContentPane(pane); 
+        
+        this.setSize(800,450);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] a){
+        new FenetrePlateau();
+    }
+}

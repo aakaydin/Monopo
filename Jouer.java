20280c5cf4052a0b034421dfc29268764662ab3e
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-03-28 23:34:51 +0200

    Update Jouer.java

--- Jouer.java
+++ Jouer.java
@@ -1,23 +1,31 @@
+import java.awt.* ;
+import javax.swing.*;
+import java.util.*;
+
 public class Jouer{
     
-    Joueur joueurCourant; 
+    Joueur joueurCourant = new Joueur("Camille",0,0); //pour les tests 
     int position;
     //boolean joueEncore = true;  
     boolean rejouera = false; 
-    Case caseCourante; 
+    Case caseCourante ;
+	//Principal
+	Plateau plateau = new Plateau(); //il faudra ensuite utiliser celui de Partie
+	JPanel panelCase ;
+	JPanel lancerLesDes = new JPanel();
     
     
     //Constructeur 
-    public Jouer(MenuPrincipal partie){
-        joueurCourant = partie.getJoueurCourant(); 
+    public Jouer(){
+        //joueurCourant = partie.getJoueurCourant(); 
+        JButton lancerDes = new JButton("Lancer les des");
+        lancerDes.addActionListener(new EcouteurLancerLesDes(joueurCourant));
+        lancerLesDes.add(lancerDes);
         
     }
     
-    
-    //Un tour : 
-    public void Tour(){
-     
-     joueurCourant.setSommeDes(0); 
+    public void TesterPrison(){
+		joueurCourant.setSommeDes(0); 
      
      
     //On teste l'état du joueur : en prison ou non 
@@ -29,9 +37,10 @@ public class Jouer{
             
             if(reponse == true){
                 joueurCourant.setEnPrison(false); 
-                joueurCourant.lancerLesDes(); 
             }else if (reponse == false){
-                joueurCourant.lancerLesDes(); 
+                //faire setMethode de la classe principale pour que le Panel changeant soit celui de lancer des dés 
+                //si on ne clique pas sur le bouton est-ce qu'on passe quand même aux lignes suivantes 
+                //est-ce qu'on fait un boolean a choisi puis un while tant que le joueur n'a pas lancer les des ... 
                 
                 if(joueurCourant.getDe1() == joueurCourant.getDe2()){
                     joueurCourant.setEnPrison(false); 
@@ -42,62 +51,97 @@ public class Jouer{
                     joueurCourant.resteEnPrison();
                     /*joueEncore = false; */ //inutile pour le moment
                 }
+                //afficher sur le panel changeant si on est sorti de prison où non 
             }
         }
     }
-    
-    
-    if(joueurCourant.getEnPrison() == false){
+}
+	
+		
+		
+    //Un tour : 
+    public void Tour(){
+     
+    joueurCourant.setSommeDes(0); 
+	
+	if(joueurCourant.getEnPrison() == false){
         if(joueurCourant.getSommeDes() == 0 ){
-            joueurCourant.lancerLesDes(); 
+			//faire setMethode de la classe principale pour que le Panel changeant soit celui de lancer des dés 
+                //si on ne clique pas sur le bouton est-ce qu'on passe quand même aux lignes suivantes 
+                //est-ce qu'on fait un boolean a choisi puis un while tant que le joueur n'a pas lancer les des ... 
         }
         
-        if(joueurCourant.getDe1() == joueurCourant.getDe2()){
+        /*if(joueurCourant.getDe1() == joueurCourant.getDe2()){
             rejouera = true; 
-        }
+            //on peut donc rejouer quand on sort de prison ? 
+        }*/
         
-        joueurCourant.avancer(joueurCourant.getSommeDes()); 
+        //joueurCourant.avancer(joueurCourant.getSommeDes()); 
+        int position = joueurCourant.getPos();
+        LinkedList<Case> mesCases = plateau.getCases();
+		caseCourante = mesCases.get(position);
         
-        caseCourante = joueurCourant.getCaseCourante(); //cette méthode doit-elle être dans Joueur?
+   
         
         /*Si on pouvait getter toutes les cases du plateau ce serait bien. Comme ça par exemple pour cagnotte on pourrait directement reverser l'argent*/ 
         /*Faire tous les affichages, faire tous les boutons : pour les différentes cases (pas que chance)*/
         if (caseCourante instanceof CaseAllerPrison){ 
             caseCourante = (CaseAllerPrison) caseCourante;
+            panelCase = caseCourante.getPanel();
             joueurCourant.allerEnPrison(); //problème de downcast? 
         }
         
         if (caseCourante instanceof CaseTaxe){
-            caseCourante = (CaseTaxe) caseCourante;
+			//ces lignes de code serait plutot dans la classe caseTaxe 
+            CaseTaxe caseCourante = (CaseTaxe) caseCourante;
+            caseCourante.setDescriptionPanel(joueurCourant);
+            panelCase = caseCourante.getPanel();
             int val = caseCourante.getValAPayer(); 
-            Plateau plateau = partie.getPlateau(); //à mettre dans MenuPrincipal 
-            CaseCagnotte caseCagnotte = plateau.getCaseCagnotte(); 
-            caseCagnotte.ajoutCagnotte(val , joueurCourant); 
+            caseCourante.setDescriptionPanel(joueurCourant);
+            panelCase = caseCourante.getPanel();
+            //pour getter les cases il va falloir les numéroter
+            
+            ((CaseCagnotte)plateau.getCases().get(14)).ajoutCagnotte(val);
         }
         
         if(caseCourante instanceof CaseCagnotte){
-            caseCourante = (CaseCagnotte) caseCourante;
+            CaseCagnotte caseCourante = (CaseCagnotte) caseCourante;
+            caseCourante.setPanel() ;
+            panelCase = caseCourante.getPanel();
             caseCourante.recupereCagnotte(joueurCourant); 
         }
         
         if(caseCourante instanceof CaseChance){ 
-            caseCourante = (CaseChance) caseCourante; 
-            caseCourante.tirerCarte(joueurCourant);
+            CaseChance caseCourante = (CaseChance) caseCourante;
+            CarteChance carteChance = caseCourante.creerCarteChance(joueurCourant);
+            carteChance.tirerCarte(); 
+			panelCase = carteChance.getPanel();
         }
         
         
         if(caseCourante instanceof CaseDepart){
-            caseCourante = (CaseDepart) caseCourante;
+            CaseDepart caseCourante = (CaseDepart) caseCourante;
+            panelCase = caseCourante.getPanel();
             caseCourante.action(joueurCourant);
         }
+        //il faut rajouter si on tombe sur la case prison
+        if(caseCourante instanceof CasePrison){
+			CasePrison caseCourante = (CasePrison) caseCourante;
+			panelCase = caseCourante.getPanel();
+			joueurCourant.setEnPrison(true);
+		}
         
+        if(caseCourante instanceof CaseProp){
+			CaseProp caseCourante = (CaseProp) caseCourante ;
+			caseCourante.setDescriptionPanel(joueurCourant);
+			panelCase = caseCourante.getPanel() ; 
+		}
+            
+		
+		 
             
             
-            
-            
-        
-    joueurCourant.lancerLesDes(); 
-    position = joueurCourant.getPos(); 
+       
     
     
     

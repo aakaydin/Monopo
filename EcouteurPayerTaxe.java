e5205b3be73c7a1a90bbbcf106ec2444861e8af6
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-03-28 23:29:07 +0200

    Add files via upload

---
+++ EcouteurPayerTaxe.java
@@ -0,0 +1,19 @@
+import java.awt.event.* ;
+import java.util.* ; 
+
+public class EcouteurPayerTaxe implements ActionListener{
+	
+	Joueur j ; 
+	int somme ;
+	
+	public EcouteurPayerTaxe(Joueur jj, int somme){
+		j = jj ;
+		this.somme = somme ;
+	}
+	
+	public void actionPerformed(ActionEvent ae){
+		//peut être que le code de cette méthode peut se touver comme c'est déjà fait dans le programme jouer 
+		j.setArgent(-somme);
+		//il faut que l'argent soit déverser dans la classe cagnotte 
+	}
+}

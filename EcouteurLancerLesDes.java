d62d6d886f9161b71ff5b0a82c3c8e42ac933b98
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-03-29 21:06:57 +0200

    Update EcouteurLancerLesDes.java

--- EcouteurLancerLesDes.java
+++ EcouteurLancerLesDes.java
@@ -17,10 +17,17 @@ public class EcouteurLancerLesDes implements ActionListener{
 		fen.aff.setDes(j.de1 , j.de2);
 		fen.aff.repaint();
 		if(!j.getEnPrison()){
-			int numCaseAAvancer = j.getSommeDes() ;
-			//je considère que 30 est le nombre de cases du plateau 
+			if(!j.getEnPrison()){
+				int numCaseAAvancer = j.getSommeDes() ;
+				//je considère que 30 est le nombre de cases du plateau 
 			
-			j.avancer(numCaseAAvancer) ; 
+				j.avancer(numCaseAAvancer) ; 
+				//appeler méthode traitement case dans le cas où je ne suis pas en prison 
+				jouer.Tour() ; 
+			}else{
+				//appeler méthode traitement quand je suis en prison
+				//jouer.traitementEstEnPriosn();
+			}
 		}
 		
 		System.out.println( j.de1 );

98a8184ce82490d879f086fd397404afc5d22513
etosoni <37653267+etosoni@users.noreply.github.com>
2018-04-01 22:41:39 +0200

    Add files via upload

---
+++ EcouteurDebuterPartie.java
@@ -0,0 +1,77 @@
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*; 
+import java.awt.*; 
+
+public class EcouteurDebuterPartie implements ActionListener {
+	
+	private MenuPrincipal2 m2; 
+	private LinkedList<String> Nom = new LinkedList<String>();  
+	private LinkedList<JTextField> text;// = new LinkedList<JTextField>();  
+	private int nbJoueur; 
+	private LinkedList<Joueur> ListJoueur= new LinkedList<Joueur>();
+	private Joueur jcourant; 
+	 
+
+	
+	public EcouteurDebuterPartie(MenuPrincipal2 f){ 
+		m2=f; 
+		nbJoueur = m2.getNbJoueur(); 
+		
+	} 
+	
+	public void actionPerformed(ActionEvent e){ 
+		//créaton de l'interface à compléter : 
+		text = m2.getTextField(); 
+		for(JTextField t : text){							//je complète le tableau des noms des joueurs
+			Nom.add(t.getText()); 
+		}
+		
+		for(String nom : Nom){ 
+			ListJoueur.add(new Joueur(nom,100000,0));
+			System.out.println(nom); 
+		}
+		
+		
+		
+		
+		
+		//on déninit le joueur courant
+		
+		int rangJoueur = 0;
+		jcourant = ListJoueur.get(rangJoueur);
+		jcourant.setSonTour(true); 
+		
+		//afficage du jeu
+		Plateau plateau = new Plateau() ; 
+		FenetreInterface fen = new FenetreInterface(1200,600,plateau,jcourant) ; 
+		
+		//on fait un while tant que joueurCourant est vivant
+		while(jcourant.estVivant()){
+			
+			System.out.println("etat tour du joueur"+jcourant.getSonTour()); 
+			while(jcourant.getSonTour()==true){
+				
+				Jouer jouer = new Jouer(plateau, fen, jcourant);
+				fen.changerPanelJoueur(jcourant);
+				
+				
+				jouer.testerPrison();
+				System.out.println("etat tour du joueur"+jcourant.getSonTour()); 
+				//c'est l'écouteur fin de tour qui rend le tour du joueur false 
+				
+			}
+			System.out.println(""+rangJoueur); 
+			rangJoueur = (rangJoueur + 1)%nbJoueur ;
+			System.out.println(""+rangJoueur); 
+			jcourant = ListJoueur.get(rangJoueur);
+			fen.changerJoueur(jcourant);
+		}
+	
+	
+		
+		
+	}
+	
+	
+}

1f31f76d870dfa014cce2fcc11a83c184fec648a
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-26 09:47:51 +0200

    Update EcouteurBtnAchatHotel.java

--- EcouteurBtnAchatHotel.java
+++ EcouteurBtnAchatHotel.java
@@ -16,21 +16,29 @@ public class EcouteurBtnAchatHotel implements ActionListener {
     }
     
     public void actionPerformed(ActionEvent ae){
-        int res = fen.getEntreeNum(); 
-        fen.setNumPropri(res); 
-        fen.dispose();
+        boolean num = fen.getEntreeNumNature(); 
+        int res; 
+        if(num == false){
+			FenetreErreurAchatBatiment f = new FenetreErreurAchatBatiment(9);
+			fen.dispose();
+		}else{
+			res = fen.getEntreeNumInt(); 
+			fen.setNumPropri(res); 
+			fen.dispose();
+			
+			
+			//Appliquer les méthodes à la caseProp de ce numéro (un attribut de Case) 
+			 
+			
+			System.out.println("Recuperation numero prop " + res); 
+			
+			fenInt.achatHotel(res);
+			
+			
+			Hotel h = new Hotel(res , plateau); 
+			fenInt.getJoueur().ajouterHotel(h);
+		}
         
-        
-        //Appliquer les méthodes à la caseProp de ce numéro (un attribut de Case) 
-         
-        
-        System.out.println("Recuperation numero prop " + res); 
-        
-        fenInt.achatHotel(res);
-        
-        
-        Hotel h = new Hotel(res , plateau); 
-        fenInt.getJoueur().ajouterHotel(h);
     }
 
 }

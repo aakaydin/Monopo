1ef2575de7477d88c8073c26f306e82c80ae0529
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-26 09:46:52 +0200

    Update EcouteurBtnAchatMaison.java

--- EcouteurBtnAchatMaison.java
+++ EcouteurBtnAchatMaison.java
@@ -16,21 +16,28 @@ public class EcouteurBtnAchatMaison implements ActionListener {
     }
     
     public void actionPerformed(ActionEvent ae){
-        int res = fen.getEntreeNum(); 
-        fen.setNumPropri(res); 
-        fen.dispose();
-        
-        
-        //Appliquer les méthodes à la caseProp de ce numéro (un attribut de Case) 
-         
-        
-        System.out.println("Recuperation numero prop " + res); 
-        
-        fenInt.achatMaison(res);
-        
-        
-        MaisonVerte m = new MaisonVerte(res , plateau); 
-        fenInt.getJoueur().ajouterMaison(m);
+       boolean num = fen.getEntreeNumNature(); 
+       int res; 
+       if(num == false){
+           FenetreErreurAchatBatiment f = new FenetreErreurAchatBatiment(9);
+           fen.dispose();
+       }else{
+           res = fen.getEntreeNumInt(); 
+           fen.setNumPropri(res); 
+           fen.dispose();
+
+
+           //Appliquer les méthodes à la caseProp de ce numéro (un attribut de Case) 
+
+
+           System.out.println("Recuperation numero prop " + res); 
+
+           fenInt.achatMaison(res);
+
+
+           MaisonVerte m = new MaisonVerte(res , plateau); 
+           fenInt.getJoueur().ajouterMaison(m); 
+       }
     }
 
 }

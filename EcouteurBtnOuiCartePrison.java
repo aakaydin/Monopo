686fbac7749af57fbee1efa5bb8207b5d4e282bf
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 19:19:31 +0200

    Update EcouteurBtnOuiCartePrison.java

--- EcouteurBtnOuiCartePrison.java
+++ EcouteurBtnOuiCartePrison.java
@@ -4,15 +4,12 @@ import java.awt.*;
 import javax.swing.*;
 import java.util.*;
 
-
+//si je commence un tour en prison
 public class EcouteurBtnOuiCartePrison implements ActionListener {
 
-    private MaFenetreCartePrison fen; 
+    private MaFenetreCartePrison fen;
     private FenetreInterface fi ;
-    
-    
-    
-    
+
     public EcouteurBtnOuiCartePrison(MaFenetreCartePrison fen, FenetreInterface fi){
         this.fen = fen; 
         this.fi = fi; 
@@ -20,9 +17,10 @@ public class EcouteurBtnOuiCartePrison implements ActionListener {
     }
     
     public void actionPerformed(ActionEvent ae){
+        //changement des attributs du joueur 
         fen.getJoueur().setEnPrison(false);
         fen.getJoueur().setCarteSortirPrison(false);
-        
+        //le joueur doit pouvoir continuer son tour comme si il avait commencé hors de prison
         fi.setTextInfo("Vous sortez de prison, lancez les des"); 
         fi.pack();
         fen.dispose();

1be9b14e6d6036c3b4c5ad6a18c7be5d83e366de
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 18:54:22 +0200

    Update CaseTaxe.java

--- CaseTaxe.java
+++ CaseTaxe.java
@@ -3,43 +3,37 @@ import javax.swing.*;
 
 public class CaseTaxe extends Case{
 
-    //Attributs
-    int valAPayer; 
-    JPanel panel = new JPanel(new BorderLayout()) ;
+    	//Attributs
+    	int valAPayer; 
+    	JPanel panel = new JPanel(new BorderLayout()) ;
     
-    //Constructeur 
-    public CaseTaxe(String nom , int position, int valeur){
-        super(position, "Case taxe");
-        valAPayer = valeur; 
+    	//Constructeur 
+   	public CaseTaxe(String nom , int position, int valeur){
+       		super(position, "Case taxe");
+       		valAPayer = valeur; 
 
         
-    }
+    	}
     
-    public int getValAPayer(){
-        return valAPayer;
-    }
+    	public int getValAPayer(){
+        	return valAPayer;
+   	}
     
-    public JPanel getPanel(){
+   	public JPanel getPanel(){
 		return panel ;
 	}
 	
+	//permet de connecter le bouton payer au joueur se trouvant sur la case pour changer la somme du bon joueur 
 	public void setDescriptionPanel(Joueur j, FenetreInterface f){
 		panel.removeAll();
 		JLabel lab = new JLabel("TAXE ! Vous devez payer "+valAPayer);
-        panel.add(lab, BorderLayout.NORTH);
-        JButton payer = new JButton("Payer");
-        payer.addActionListener(new EcouteurPayerTaxe(j, -valAPayer, f,payer));
-        panel.add(payer, BorderLayout.SOUTH);
-        panel.repaint();
+       		panel.add(lab, BorderLayout.NORTH);
+        	JButton payer = new JButton("Payer");
+        	payer.addActionListener(new EcouteurPayerTaxe(j, -valAPayer, f,payer));
+       		panel.add(payer, BorderLayout.SOUTH);
+       		panel.repaint();
 	}
 		
-    
-    //Est finalement dans la classe Jouer
-    //faire action sur le joueur 
-    /*public void paieTaxe(Joueur joueur){
-        jouer.setArgent(-valAPayer);
-        //récupérer la case cagnotte du plateau et augmenter sa somme
-        caseCagnotte.ajoutCagnotte(valAPayer);
-        */
+
 
 }

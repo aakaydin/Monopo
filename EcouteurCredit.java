c98423749009ea65cddf835de7b876c6b758bdfd
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 19:21:49 +0200

    Update EcouteurCredit.java

--- EcouteurCredit.java
+++ EcouteurCredit.java
@@ -5,19 +5,20 @@ import java.util.* ;
 
 public class EcouteurCredit implements ActionListener{
 	
-	
-	
+	//le menu banque est la fenêtre dans laquelle se trouve le bouton associé à cet écouteur 
 	private MenuBanque mb;
 	private JTextField t1;
 	private JTextField t2; 
 	private String Nom; 
 	private int MontantTransfere; 
+	//joeur qui veut faire l'opération banquaire
 	private Joueur jcourant;
+	//liste des joueurs de la partie 
 	private LinkedList<Joueur> ListJoueur; 
 	private FenetreInterface fen; 
 	
-	
 	public EcouteurCredit(MenuBanque m){
+		//on récupère les différentes données depuis la fenêtre interface 
 		mb = m; 
 		jcourant = mb.getJoueurCourant(); 
 		ListJoueur = m.getListJoueur(); 
@@ -26,12 +27,8 @@ public class EcouteurCredit implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent ae){
-		
-		
+		//ouverture de la fenêtre qui permet de demander un crédit 
 		MenuCredit mc = new MenuCredit(400,300,jcourant,fen); 
-		
-		
-		
-		 
+	 
 	}
 }

e9f8ac8d770d34adbbc65ac404f718f246ae2c08
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 18:48:29 +0200

    Update CaseChance.java

--- CaseChance.java
+++ CaseChance.java
@@ -7,7 +7,7 @@ public class CaseChance extends Case {
 		
 	}
 	
-	//première méthode inutile, juste au cas où
+	//méthode qui va créer une carteChance lorsqu'on arrive sur la case avec en paramètre le joueur et la FenetreInterface pour pouvoir intéragir avec eux 
 	public CarteChance creerCarteChance(Joueur j, FenetreInterface f){
 		CarteChance cartelucky = new CarteChance(j,f);
 		return cartelucky; 

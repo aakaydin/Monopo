9be821f9dcfaa12be8d77290dacae8018df73f8c
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 19:16:10 +0200

    Update EcouteurBtnNonCartePrison.java

--- EcouteurBtnNonCartePrison.java
+++ EcouteurBtnNonCartePrison.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.util.*;
 
+//quand je suis en prison et que je possède une carte prison, une fenêtre apparait pour me proposer de l'utiliser, elle possède deux boutons oui et non 
 public class EcouteurBtnNonCartePrison implements ActionListener{
 
     private MaFenetreCartePrison fen; 
@@ -12,14 +13,18 @@ public class EcouteurBtnNonCartePrison implements ActionListener{
    
     
     public EcouteurBtnNonCartePrison(MaFenetreCartePrison fen, FenetreInterface fi){
+
         this.fen = fen; 
         this.fi = fi;
         
     }
-        
+    
     public void actionPerformed(ActionEvent ae){
+        //l'affichage du jeu va alors être mis en place comme si le joueur n'avait pas eu la carte 
         fi.setTextInfo("Lancez les des et faites un double pour sortir");
         fi.pack() ;
+        //fermeture de la fenêtre de choix 
         fen.dispose();
+        
     }
 }

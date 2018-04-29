a31b5ce4c1ae2dc779272153009a1d5924037760
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 18:52:08 +0200

    Update CasePrison.java

--- CasePrison.java
+++ CasePrison.java
@@ -1,20 +1,15 @@
 import java.awt.*;
 import javax.swing.*;
 
+//classe qui ne concerne que le traitement à faire quand on arrive en prison en lancant les dés et en tombant sur la case prison 
 public class CasePrison extends Case{
 	
-    JPanel panel = new JPanel();
-        
-    public CasePrison(){
-        super(7 , "Prison"); 
-        JLabel arriveprison = new JLabel("Pas de chance, vous tombez en prison");
-        panel.add(arriveprison);
-    }
-    
-	
-    //il va y avoir deux cas de Panel différent 
-	//j'ai l'impression que l'affichage dans le cas du déjà en prison a été pris en compte par Lisa, il fadra discuter de où on met le code
-	//soit on fait en sorte de ne récupérer que lui et l'autre partie du code (beaucoup plus conséquente) est dans la classe jouer soit on récupère les deux et on place une condition en paramètre sur l'état du joueur
+	JPanel panel = new JPanel();
+	public CasePrison(){
+		super(7 , "Prison"); 
+		JLabel arriveprison = new JLabel("Pas de chance, vous tombez en prison");
+		panel.add(arriveprison);
+    	}
 		
 	public JPanel getPanel(){
 		return panel ;

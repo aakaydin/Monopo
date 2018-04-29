1257cc43adb85b678d21255145e164e3adaeae53
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 19:01:12 +0200

    Update EcouteurAcheter.java

--- EcouteurAcheter.java
+++ EcouteurAcheter.java
@@ -2,10 +2,14 @@ import java.awt.event.* ;
 import java.util.* ; 
 import javax.swing.*;
 
+//classe utilisée pour acheter une propriété 
 public class EcouteurAcheter implements ActionListener{
 	
-	Joueur j ; 
+	//joueur se trouvant sur la case 
+	Joueur j ;
+	//case associée qui va potentiellement être achetée 
 	CaseProp c ;
+	//fenêtre interface pour faire la mise à jour de l'interface d'une manière coordonnée avec l'action du joueur 
 	public FenetreInterface fen ;
 	JButton but;
 	
@@ -15,29 +19,31 @@ public class EcouteurAcheter implements ActionListener{
 		but = button;
 		
 	}
-	
+	//permet de faire le réglage de la fenêtre interface
 	public void setFenetreInterface(FenetreInterface f){
 		fen = f ;
 	}
-		
+	//permet de faire le réglage du joueur 	
 	public void setJoueur(Joueur jj){
 		j = jj;
 	}
 		
-		
-		
+	//méthode d'achat
 	public void actionPerformed(ActionEvent ae){
 		
+		//on ne peut acheter qu'une fois la propriété
 		but.setEnabled(false);
-		
+		//changements joueur : somme d'argent du joueur et ajout de la propriété à la liste de ses propriétés 
 		j.setArgent(-c.getPrix());
 		j.getMesProprietes().add(c); 
+		//changements sur la case 
 		c.setProprietaire(j);
 		c.setAchete(true);
+		//changement de l'affichage 
 		fen.changerPanelJoueur(j);
 		fen.plateau.getPanelCase( j.getPos()).setCouleurProp( j.col);
 		fen.plateau.repaint();
-		//fen.getPanelSouth().repaint();
+		
 		
 	}
 }

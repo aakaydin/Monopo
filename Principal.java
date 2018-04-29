4f66b5fce32e728c96f58e31174bb14f9c6a82ca
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 23:45:30 +0200

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,45 +1,12 @@
 import java.util.*;
+
+//méthode à executer pour commencer notre jeu 
 public class Principal { 
 	
 	public static void main(String[] args){
 		
-		//MenuPrincipal m1 = new MenuPrincipal(400,300);
-		//MenuPrincipal2 m2 = m1 ;
-		/*LinkedList<Joueur> mesJoueurs = new LinkedList<Joueur>() ;
-		Joueur j1 = new Joueur("A");
-		Joueur j2 = new Joueur("L");
-		mesJoueurs.add(j1);
-		mesJoueurs.add(j2);
-		
-		int nbJoueurs = mesJoueurs.size() ; 
-		
-		Joueur jcourant;
-		
-		Plateau plateau = new Plateau() ; 
-		FenetreInterface fen = new FenetreInterface(1200,600,plateau) ; 
-		
-		//on déninit le joueur courant
-		
-		int rangJoueur = 0;
-		jcourant = mesJoueurs.get(rangJoueur);
-		
-		//on fait un while tant que joueurCourant est vivant
-		while(jcourant.estVivant()){
-			
-			while(jcourant.getSonTour()){
-				Jouer jouer = new Jouer(plateau, fen, jcourant);
-				fen.changerPanelJoueur(jcourant);
-				
-				jouer.testerPrison();
-				//c'est l'écouteur fin de tour qui rend le tour du joueur false 
-			}
-			
-			rangJoueur = (rangJoueur + 1)%nbJoueurs ;
-			jcourant = mesJoueurs.get(rangJoueur);
-			fen.changerJoueur(jcourant);
-			
-		} */ 
-		
+		//création d'une fenêtre pour rentrer le nombre de joueurs et la présence d'une intelligence artificielle
+		//le jeu sera lancé par des méthodes présentent dans les boutons des fen^tres de mises en place du jeu 
 		MenuPrincipal m1 = new MenuPrincipal(400,300);	
 		
 	}

66d17d9ec60398f5898a617ce7345e1320950f95
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 22:51:36 +0200

    Update MaisonVerte.java

--- MaisonVerte.java
+++ MaisonVerte.java
@@ -3,47 +3,42 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MaisonVerte{ 
-	//modifier attribut position; 
-	private int prix;
-	private int prixHypotheque;
-	private int position;
-	//private int positionX; 
-	//private int positionY;  
-	//private CasePropriete Propriete;
-    private LinkedList<Case> listeCases;
-    private CaseProp caseAssociee;
 	
-	public MaisonVerte(int position,int prix,int PHypotheque){  //propriete){
+	//prix de la maison
+	private int prix;
+	//prix de vente de la maison
+	private int prixHypotheque;
+	//numéro de la case sur laquelle se trouve la maison
+	private int position;
+	
+	//liste des cases du plateau
+    	private LinkedList<Case> listeCases;
+	//case associée à la maison
+    	private CaseProp caseAssociee;
+	
+	public MaisonVerte(int position,int prix,int PHypotheque){  
 		this.position = position;
 		this.prix = prix;
 		this.prixHypotheque = PHypotheque; //chaque maison a un prix d'hypotheque different selon la propriete
-		//Propriete = propriete; //chaque maison a un prix different selon la propriete
+		e
 	} 
     
-    public MaisonVerte(int position , Plateau p){
-        this.position = position; 
-        
-        listeCases = p.getCases(); 
-        Case c = listeCases.get(position); 
-        
-        if (c instanceof CaseProp){
-            caseAssociee = (CaseProp) c;
-        
-            prix = caseAssociee.getPrix()/10; 
-            prixHypotheque = prix/2; 
-            
-        }else{
-            System.out.println("On ne peut mettre de maison que sur les cases propriétés");
-        }
-    }
-	
-	/*public int getX(){
-		return positionX; 
-	} 
-	
-	public int getY(){
-		return positionY; 
-	} */
+	public MaisonVerte(int position , Plateau p){
+		//initialisation de la poistion de la maison
+		this.position = position;
+		//récupéartion de la liste des cases du plateau 
+		listeCases = p.getCases(); 
+		//récupéartion de la case associée 
+		Case c = listeCases.get(position); 
+		//si il s'agit bien d'une case propriété alors il faut que je change les attributs de ma maison
+		if (c instanceof CaseProp){
+  			caseAssociee = (CaseProp) c;
+    			prix = caseAssociee.getPrix()/10; 
+    			prixHypotheque = prix/2; 
+		}else{ //sinon rien ne se passe, mais le message d'erreur qui est associé est prise en compte dans ecouteurBtnMaison
+    			System.out.println("On ne peut mettre de maison que sur les cases propriétés");
+		}
+	}
 	
 	public int getPosition(){
 		return position; 

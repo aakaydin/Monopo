8b67595e4d2f6399bfd81ca65e158641e123999b
camcamchamrousse <37652985+camcamchamrousse@users.noreply.github.com>
2018-04-29 22:15:55 +0200

    Update EcouteurSuivant1.java

--- EcouteurSuivant1.java
+++ EcouteurSuivant1.java
@@ -8,8 +8,8 @@ public class EcouteurSuivant1 implements ActionListener {
 	private int nbJoueur; 
 	private int nbIA; 
 	
-
-	
+	//cet écouteur va permettre de passer du premier menu ou on indique le nombre du joueur et la présence d'une interface graphique 
+	//au deuxième menu où on rentre les noms des différents joueurs
 	public EcouteurSuivant1(MenuPrincipal f, MenuPrincipal2 m2){ 
 		super();
 		m1=f; 
@@ -19,21 +19,24 @@ public class EcouteurSuivant1 implements ActionListener {
 	
 	public void actionPerformed(ActionEvent e){ 
 		
-		
+		//il faut vérifier que la case où on rentre le nombre de joueurs n'est pas vide 
 		if(m1.getnbJoueur().getText()!= null){
 			nbJoueur = Integer.parseInt(m1.getnbJoueur().getText()); 
 		} 
-		
+		//si on a une intelligence artificielle il faut l'ajouter dans la partie 
 		if(m1.getIA()){
 			nbIA = 1; 
 		} 
+		//on règle le nombre de joueurs 
 		m2.setNbJoueur(nbJoueur); 
+		//on règle le nombre d'IA qui pour l'instant ne peut être égal qu'à 0 ou 1
 		m2.setNbIA(nbIA); 
 		m2.FinishFrame(nbJoueur, nbIA); 
 		m2.setVisible(true); 
 		m1.setVisible(false); 
 	}
 	
+	//getteurs 
 	public int getnbJoueur(){
 		return nbJoueur; 
 	}
